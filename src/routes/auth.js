5876c2c2f3a1e8dbfeacd595aa14d0d5cd95f084
RahmanAdhitya <rahman.adhitya23@gmail.com>
2022-06-21 21:40:02 +0700

    [P3-2] register feature and [P3-3]verification email address

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,7 +4,7 @@ const { authorizedToken } = require("../middleware/authMiddleware")
 const { registerValidation, validator, adminValidation } = require("../middleware/validator/validator")
 const authService = require("../services/auth/index.js")
 
-router.post("/register", controllers(authService.register))
+router.post("/register", registerValidation, validator, controllers(authService.register))
 router.get("/verify/:token", controllers(authService.verifyEmail))
 
 
