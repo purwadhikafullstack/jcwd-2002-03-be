f8b21a181f954e57a4373697b6014e0d7c1a2e33
akimajid <akimajid07@gmail.com>
2022-06-23 17:47:44 +0700

    [FIX] fix bug error after req

--- src/middleware/controllers.js
+++ src/middleware/controllers.js
@@ -1,22 +1,27 @@
+const controllers = (services) => {
+  return async (req, res) => {
+    try {
+      const serviceControllers = await services(req);
 
-const controllers = ((services) => {
-    return async (req, res) => {
-        try {
-            const serviceControllers = await services(req)
+      if (!serviceControllers.success) throw serviceControllers;
 
-            if (!serviceControllers.success) throw serviceControllers;
-            return res.status(serviceControllers.statusCode || 200).json({
-                message: serviceControllers.message,
-                result: serviceControllers.data
-            }).redirect(serviceControllers.redirect)
-        } catch (err) {
-            console.log(err)
-            return res.status(err.statusCode).json({
-                message: err.message
-            })
-        }
+      if (serviceControllers.redirect) {
+        return res.redirect(serviceControllers.redirect);
+      }
+
+      return res
+        .status(serviceControllers.statusCode || 200)
+        .json({
+          message: serviceControllers.message,
+          result: serviceControllers.data,
+        })
+    } catch (err) {
+      console.log(err);
+      return res.status(err.statusCode).json({
+        message: err.message,
+      });
     }
-})
+  };
+};
 
-
-module.exports = controllers
+module.exports = controllers;
