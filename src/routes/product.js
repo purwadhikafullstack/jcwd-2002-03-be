528ec04d075ebb048dd4b90e4dd3ab5cc954252c
Arlonas <98306152+Arlonas@users.noreply.github.com>
2022-06-27 14:12:50 +0700

    feat: added some changes

--- src/routes/product.js
+++ src/routes/product.js
@@ -3,5 +3,6 @@ const controllers = require("../middleware/controllers");
 const productService = require("../services/product");
 
 router.get("/", controllers(productService.getProduct));
+router.post("/", )
 
 module.exports = router;
