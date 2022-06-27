da9e311a258e6924623e6c970b4e4ad11633862f
Arlonas <98306152+Arlonas@users.noreply.github.com>
2022-06-27 15:00:52 +0700

    rebase

--- src/lib/uploader.js
+++ src/lib/uploader.js
@@ -2,11 +2,7 @@ const multer = require("multer");
 const { nanoid } = require("nanoid");
 
 const fileUploader = ({
-<<<<<<< HEAD
   destinationFolder = "profile_pictures",
-=======
-  destinationFolder = "posts",
->>>>>>> 9d8c2003d40579a0d95987d4ce73c7527ccf27b7
   prefix = "POST",
   fileType = "image",
 }) => {
