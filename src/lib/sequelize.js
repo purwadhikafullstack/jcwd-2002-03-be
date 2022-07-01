7898325db7243f106dc1f72b27677d73003e6d52
akimajid <akimajid07@gmail.com>
2022-07-01 15:28:40 +0700

    Feat/[P3-39] fetch product with pagination admin, [P3-41] sorting product by name and/or price admin (#16)
    * Feat: merge BE from main
    
    * Feat: merge BE from main
    
    * Feat: added reset and change password API
    
    * Fix: clean code line
    
    * [FIX] fix bug error after req
    
    * FIX: rebase
    
    * FIX: rebase
    
    * feat: added fetch product and sorting api
    
    * FIX: rebase
    
    * fix: enable alter in sequelize.sync

--- src/lib/sequelize.js
+++ src/lib/sequelize.js
@@ -1,53 +1,55 @@
-const { Sequelize } = require('sequelize');
-const mysqlConfig = require('../config/database');
+const { Sequelize } = require("sequelize");
+const mysqlConfig = require("../config/database");
 
 const sequelize = new Sequelize({
-    username: mysqlConfig.MYSQL_USERNAME,
-    password: mysqlConfig.MYSQL_PASSWORD,
-    database: mysqlConfig.MYSQL_DB_NAME,
-    port: 3306,
-    dialect: 'mysql',
-    logging: false,
+  username: mysqlConfig.MYSQL_USERNAME,
+  password: mysqlConfig.MYSQL_PASSWORD,
+  database: mysqlConfig.MYSQL_DB_NAME,
+  port: 3306,
+  dialect: "mysql",
+  logging: false,
 });
 
 // models
-const Admin = require("../models/admin")(sequelize)
-const Address = require("../models/address")(sequelize)
-const Buy_stock = require("../models/buy_stock")(sequelize)
-const Cart = require("../models/cart")(sequelize)
-const Inventory = require("../models/inventory")(sequelize)
-const Product = require("../models/product")(sequelize)
-const User = require("../models/user")(sequelize)
-const VerificationToken = require('../models/verificationEmail')(sequelize);
-const ForgotPasswordToken = require('../models/forgotPassword')(sequelize);
-const Stock_order = require('../models/stock_order')(sequelize)
-const Stock_opname = require("../models/stock_opname")(sequelize)
-const Stock_sold = require("../models/stock_sold")(sequelize)
-const Product_image = require("../models/product_image")(sequelize)
-const Transaction = require("../models/transaction")(sequelize)
-const Transaction_items = require("../models/transaction_items")(sequelize)
-const Payment = require('../models/payment')(sequelize)
+const Admin = require("../models/admin")(sequelize);
+const Address = require("../models/address")(sequelize);
+const Buy_stock = require("../models/buy_stock")(sequelize);
+const Cart = require("../models/cart")(sequelize);
+const Category = require("../models/category")(sequelize);
+const Inventory = require("../models/inventory")(sequelize);
+const Product = require("../models/product")(sequelize);
+const User = require("../models/user")(sequelize);
+const VerificationToken = require("../models/verificationEmail")(sequelize);
+const ForgotPasswordToken = require("../models/forgotPassword")(sequelize);
+const Stock_order = require("../models/stock_order")(sequelize);
+const Stock_opname = require("../models/stock_opname")(sequelize);
+const Stock_sold = require("../models/stock_sold")(sequelize);
+const Product_image = require("../models/product_image")(sequelize);
+const Transaction = require("../models/transaction")(sequelize);
+const Transaction_items = require("../models/transaction_items")(sequelize);
+const Payment = require("../models/payment")(sequelize);
 
 // Associations
 
 // 1:M
-Address.belongsTo(User)
-User.hasMany(Address)
+Address.belongsTo(User);
+User.hasMany(Address);
 
-Product_image.belongsTo(Product)
-Product.hasMany(Product_image)
+Product_image.belongsTo(Product);
+Product.hasMany(Product_image);
 
-
+Product.belongsTo(Category);
+Category.hasMany(Product);
 // M:N
-Transaction_items.belongsTo(Transaction)
-Transaction.hasMany(Transaction_items)
-Transaction_items.belongsTo(Product)
-Product.hasMany(Transaction_items)
+Transaction_items.belongsTo(Transaction);
+Transaction.hasMany(Transaction_items);
+Transaction_items.belongsTo(Product);
+Product.hasMany(Transaction_items);
 
-Cart.belongsTo(User)
-User.hasMany(Cart)
-Cart.belongsTo(Product)
-Product.hasMany(Cart)
+Cart.belongsTo(User);
+User.hasMany(Cart);
+Cart.belongsTo(Product);
+Product.hasMany(Cart);
 
 VerificationToken.belongsTo(User);
 User.hasMany(VerificationToken);
@@ -55,39 +57,40 @@ User.hasMany(VerificationToken);
 ForgotPasswordToken.belongsTo(User);
 User.hasMany(ForgotPasswordToken);
 
-Stock_order.belongsTo(Product)
-Product.hasMany(Stock_order)
-Stock_order.belongsTo(Admin)
-Admin.hasMany(Stock_order)
+Stock_order.belongsTo(Product);
+Product.hasMany(Stock_order);
+Stock_order.belongsTo(Admin);
+Admin.hasMany(Stock_order);
 
-Payment.belongsTo(Admin)
-Admin.hasMany(Payment)
-Payment.belongsTo(Transaction)
-Transaction.hasMany(Payment)
+Payment.belongsTo(Admin);
+Admin.hasMany(Payment);
+Payment.belongsTo(Transaction);
+Transaction.hasMany(Payment);
 
-Stock_opname.belongsTo(Product)
-Product.hasMany(Stock_opname)
-Stock_opname.belongsToMany(Stock_order, { through: Buy_stock })
-Stock_opname.belongsToMany(Transaction, { through: Stock_sold })
+Stock_opname.belongsTo(Product);
+Product.hasMany(Stock_opname);
+Stock_opname.belongsToMany(Stock_order, { through: Buy_stock });
+Stock_opname.belongsToMany(Transaction, { through: Stock_sold });
 
-Inventory.belongsTo(Product)
-Product.hasMany(Inventory)
-Inventory.belongsTo(Transaction)
-Transaction.hasMany(Inventory)
+Inventory.belongsTo(Product);
+Product.hasMany(Inventory);
+Inventory.belongsTo(Transaction);
+Transaction.hasMany(Inventory);
 
 module.exports = {
-    Address,
-    Admin,
-    Buy_stock,
-    Cart,
-    Inventory,
-    Product,
-    Product_image,
-    User,
-    sequelize,
-    Stock_order,
-    Stock_opname,
-    Stock_sold,
-    VerificationToken,
-    ForgotPasswordToken
-}
+  Address,
+  Admin,
+  Buy_stock,
+  Category,
+  Cart,
+  Inventory,
+  Product,
+  Product_image,
+  User,
+  sequelize,
+  Stock_order,
+  Stock_opname,
+  Stock_sold,
+  VerificationToken,
+  ForgotPasswordToken,
+};
--- src/models/product.js
+++ src/models/product.js
@@ -1,42 +1,48 @@
 const { DataTypes } = require("sequelize")
 
 const Product = (sequelize) => {
-    return sequelize.define("Product", {
-        med_name: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        no_med: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true
-        },
-        no_bpom: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true
-        },
-        selling_price: {
-            type: DataTypes.INTEGER,
-            allowNull: false
-        },
-        category: {
-            type: DataTypes.BOOLEAN,
-            allowNull: false
-        },
-        kemasan: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        indikasi: {
-            type: DataTypes.STRING,
-            allowNull: true
-        },
-        discount: {
-            type: DataTypes.FLOAT,
-            allowNull: true
-        },
-    })
-}
+  return sequelize.define("Product", {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    med_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    nomer_med: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    nomer_bpom: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    selling_price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    kemasan: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    indikasi: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    discount: {
+      type: DataTypes.FLOAT,
+      allowNull: true,
+    },
+    kandungan: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+  });
+};
 
 module.exports = Product
