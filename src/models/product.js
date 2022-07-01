c487a2ddfd8ceee3701d93ca94a0bec21819d3e8
Arlonas <98306152+Arlonas@users.noreply.github.com>
2022-07-01 18:46:06 +0700

    feat: aded filter and sorting

--- src/models/product.js
+++ src/models/product.js
@@ -1,66 +1,48 @@
-const { DataTypes } = require("sequelize");
+const { DataTypes } = require("sequelize")
 
 const Product = (sequelize) => {
-  return sequelize.define("Product", {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-      allowNull: false,
-    },
-    med_name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    nomor_med: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    nomor_bpom: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    selling_price: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    buying_price: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    discount: {
-      type: DataTypes.FLOAT,
-      allowNull: true,
-    },
-    indikasi: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    kandungan: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    kemasan: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    resep: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-    },
-    penyimpanan: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    principal: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    NIE: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  });
-};
+    return sequelize.define("Product", {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        med_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        nomer_med: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        nomer_bpom: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        selling_price: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        kemasan: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        indikasi: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        discount: {
+            type: DataTypes.FLOAT,
+            allowNull: true
+        },
+        kandungan: {
+            type: DataTypes.STRING,
+            allowNull: true
+        }
+    })
+}
 
-module.exports = Product;
+module.exports = Product
--- src/services/product/index.js
+++ src/services/product/index.js
@@ -1,10 +1,11 @@
 const Service = require("../service");
-const { Product, Product_image } = require("../../lib/sequelize");
+const { Product, Product_image, Category } = require("../../lib/sequelize");
 class productService extends Service {
   // npx nodemon . --inspect
   static getProduct = async (req) => {
     try {
       const { _sortBy = "", _sortDir = "", _limit = 5, _page = 1 } = req.query;
+      console.log(req.query._sortBy);
       delete req.query._limit;
       delete req.query._page;
       delete req.query._sortBy;
@@ -21,244 +22,18 @@ class productService extends Service {
             model: Product_image,
             attributes: ["image_url"],
           },
+          {
+            model: Category,
+            attributes: ["category_name", "id"],
+            where: req.query?.categoryId ? { id: req.query.categoryId } : {},
+          },
         ],
       });
+      console.log(findProducts);
       return this.handleSuccess({
         message: "Products found",
         statusCode: 200,
         data: findProducts,
-      });
-    } catch (err) {
-      console.log(err);
-      this.handleError({
-        message: "Server Error",
-        statusCode: 500,
-      });
-    }
-  };
-  static editNama = async (req) => {
-    try {
-      const { username } = req.body;
-      const findUser = await User.findOne({
-        where: {
-          name: username,
-        },
-      });
-      if (findUser) {
-        return this.handleError({
-          message: "username has been taken",
-          statusCode: 400,
-        });
-      }
-      await User.update(
-        {
-          name: username,
-        },
-        {
-          where: {
-            id: 1,
-          },
-        }
-      );
-      const name = await User.findOne({
-        where: {
-          id: 1,
-        },
-        attributes: ["name"],
-      });
-      return this.handleSuccess({
-        message: "your name was changed successfully",
-        statusCode: 201,
-        data: name,
-      });
-    } catch (err) {
-      console.log(err);
-      this.handleError({
-        message: "Server Error",
-        statusCode: 500,
-      });
-    }
-  };
-  static tambahTl = async (req) => {
-    try {
-      const { birthdate } = req.body;
-      const userBirthdate = await User.update(
-        {
-          birthDate: birthdate,
-        },
-        {
-          where: {
-            id: 1,
-          },
-        }
-      );
-      return this.handleSuccess({
-        message: "your birthdate was added successfully",
-        statusCode: 201,
-        data: userBirthdate,
-      });
-    } catch (err) {
-      console.log(err);
-      this.handleError({
-        message: "Server Error",
-        statusCode: 500,
-      });
-    }
-  };
-  static tambahJk = async (req) => {
-    try {
-      const { gender } = req.body;
-      console.log(gender);
-      const userGender = await User.update(
-        {
-          gender,
-        },
-        {
-          where: {
-            id: 1,
-          },
-        }
-      );
-
-      return this.handleSuccess({
-        message: "your gender was added successfully",
-        statusCode: 201,
-        data: userGender,
-      });
-    } catch (err) {
-      console.log(err);
-      this.handleError({
-        message: "Server Error",
-        statusCode: 500,
-      });
-    }
-  };
-  static editProfilePicture = async (req) => {
-    try {
-      const uploadFileDomain = process.env.UPLOAD_FILE_DOMAIN;
-      const filePath = `profile-pictures`;
-      const { filename } = req.file;
-
-      await User.update(
-        {
-          ...req.body,
-          image_url: `${uploadFileDomain}/${filePath}/${filename}`,
-        },
-        {
-          where: {
-            id: 1,
-          },
-        }
-      );
-      const imageUrl = await User.findOne({
-        where: {
-          id: 1,
-        },
-        attributes: ["image_url"],
-      });
-      return this.handleSuccess({
-        message: "your profile picture was created successfully",
-        statusCode: 201,
-        data: imageUrl,
-      });
-    } catch (err) {
-      console.log(err);
-      this.handleError({
-        message: "Server Error",
-        statusCode: 500,
-      });
-    }
-  };
-  static tambahAlamat = async (req) => {
-    try {
-      const {
-        labelAlamat,
-        nama,
-        nomorHp,
-        provinsi,
-        kotaKabupaten,
-        kecamatan,
-        alamat,
-        kodePos,
-        UserId,
-      } = req.body;
-      const address = await Address.create({
-        labelAlamat,
-        nama,
-        nomorHp,
-        provinsi,
-        kotaKabupaten,
-        kecamatan,
-        alamat,
-        kodePos,
-        UserId,
-      });
-      return this.handleSuccess({
-        message: "your address was added successfully",
-        statusCode: 201,
-        data: address,
-      });
-    } catch (err) {
-      console.log(err);
-      this.handleError({
-        message: "Server Error",
-        statusCode: 500,
-      });
-    }
-  };
-  static tambahNomorHp = async (req) => {
-    try {
-      const { phone } = req.body;
-      const userPhone = await User.update(
-        {
-          phone,
-        },
-        {
-          where: {
-            id: 1,
-          },
-        }
-      );
-      return this.handleSuccess({
-        message: "your phone number was created successfully",
-        statusCode: 201,
-        data: userPhone,
-      });
-    } catch (err) {
-      console.log(err);
-      this.handleError({
-        message: "Server Error",
-        statusCode: 500,
-      });
-    }
-  };
-  static getAddress = async (req) => {
-    try {
-      const address = await User.findAndCountAll({
-        where: {
-          id: 1,
-        },
-        include: [
-          {
-            model: Address,
-            attributes: [
-              "labelAlamat",
-              "nama",
-              "nomorHp",
-              "provinsi",
-              "kotaKabupaten",
-              "kecamatan",
-              "alamat",
-              "kodePos",
-              "id",
-            ],
-          },
-        ],
-      });
-      return this.handleSuccess({
-        message: "your address was added successfully",
-        statusCode: 201,
-        data: address.rows,
       });
     } catch (err) {
       console.log(err);
