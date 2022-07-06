85197f391c63833caae0b8e980d91144a3983324
akimajid <akimajid07@gmail.com>
2022-07-06 23:53:58 +0700

    Feat/[P3-42] (#24)

--- src/models/inventory.js
+++ src/models/inventory.js
@@ -1,29 +1,32 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 
 const Inventory = (sequelize) => {
-    return sequelize.define("inventory", {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: true,
-            primaryKey: true,
-            autoIncrement: true,
-            // productId
-            // transactionId
-        },
-        quantity: {
-            type: DataTypes.INTEGER,
-            allowNull: true,
-        },
-        expired_date: {
-            type: DataTypes.DATE,
-            allowNull: false
-        }
-        ,
-        type: {
-            type: DataTypes.STRING,
-            allowNull: true,
-        }
-    });
+  return sequelize.define("inventory", {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      primaryKey: true,
+      autoIncrement: true,
+      // productId
+      // transactionId
+    },
+    quantity: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    expired_date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    type: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    buying_price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  });
 };
 
 module.exports = Inventory;
--- src/routes/inventory.js
+++ src/routes/inventory.js
@@ -1,5 +1,8 @@
 const router = require("express").Router()
+const { authorizedToken } = require("../middleware/authMiddleware")
 const controllers = require("../middlewares/controllers")
+const inventoryService = require("../services/inventory")
 
+router.post("/add-stock", authorizedToken, controllers(inventoryService.addStockProduct))
 
 module.exports = router
