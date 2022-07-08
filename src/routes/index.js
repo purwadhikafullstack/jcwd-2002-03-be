5376e3cf42c55a82151de398611fed869a72712e
Arlonas <98306152+Arlonas@users.noreply.github.com>
2022-07-08 13:28:42 +0700

    feat: added cart (#27)

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,19 @@
-const authRoutes = require("./auth")
-const transactionRoutes = require("./transaction")
-const inventoryRoutes = require("./inventory")
-const profileRoutes = require("./profile")
-const productRoutes = require("./product")
-const categoryRoutes = require("./category")
-const paymentRoutes = require("./payment")
+const authRoutes = require("./auth");
+const transactionRoutes = require("./transaction");
+const inventoryRoutes = require("./inventory");
+const profileRoutes = require("./profile");
+const productRoutes = require("./product");
+const categoryRoutes = require("./category");
+const paymentRoutes = require("./payment");
+const cartRoutes = require("./cart");
 
 module.exports = {
-    authRoutes,
-    transactionRoutes,
-    inventoryRoutes,
-    profileRoutes,
-    productRoutes,
-    categoryRoutes,
-    paymentRoutes
-}
+  authRoutes,
+  transactionRoutes,
+  inventoryRoutes,
+  profileRoutes,
+  productRoutes,
+  categoryRoutes,
+  paymentRoutes,
+  cartRoutes,
+};
