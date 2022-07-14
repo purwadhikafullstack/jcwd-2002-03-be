e6a8a30b90d14dd89eb08c94e52aa270a884f99e
akimajid <akimajid07@gmail.com>
2022-07-14 22:16:03 +0700

    Feat: added sales report admin

--- src/services/inventory/index.js
+++ src/services/inventory/index.js
@@ -1,5 +1,11 @@
 const Service = require("../service");
-const { Product, Inventory, Stock_opname } = require("../../lib/sequelize");
+const {
+  Product,
+  Inventory,
+  Stock_opname,
+  Transaction,
+  Transaction_items,
+} = require("../../lib/sequelize");
 const { Op } = require("sequelize");
 
 class inventoryService extends Service {
@@ -35,6 +41,62 @@ class inventoryService extends Service {
       });
     } catch (err) {
       console.log;
+      this.handleError({
+        message: "Server error",
+        statusCode: 500,
+      });
+    }
+  };
+  static getSalesReport = async (req) => {
+    try {
+      const { _limit = 30, _page = 1, _sortBy = "", _sortDir = "", searchProduct, } = req.query;
+
+      delete req.query._limit;
+      delete req.query._page;
+      delete req.query._sortBy;
+      delete req.query._sortDir;
+      delete req.query.searchProduct;
+
+      let searchByNameClause = {};
+
+      if (searchProduct) {
+        searchByNameClause = {
+          med_name: { [Op.like]: `%${searchProduct}%` },
+        };
+      }
+
+      const findSalesReport = await Inventory.findAndCountAll({
+        where: {
+          ...req.query,
+        //   ...searchByNameClause,
+        },
+        limit: _limit ? parseInt(_limit) : undefined,
+        offset: (_page - 1) * _limit,
+        // order: _sortBy ? [[_sortBy, _sortDir]] : undefined,
+        order: [["createdAt", "DESC"]],
+        distinct: true,
+        include: [
+          {
+            model: Transaction,
+            include: Transaction_items,
+          },
+          {
+            model: Product,
+            where: {
+                ...req.query,
+                ...searchByNameClause,
+              },
+          }
+        ],
+      });
+
+      return this.handleSuccess({
+        message: "Sales report found",
+        statusCode: 200,
+        data: findSalesReport,
+      });
+    } catch (err) {
+      console.log(err);
       this.handleError({
         message: "Server error",
         statusCode: 500,
