1b2787fa8b26bec068443618aa5a95790936045c
akimajid <akimajid07@gmail.com>
2022-07-16 05:43:01 +0700

    Feat: added filtering by name

--- src/routes/transaction.js
+++ src/routes/transaction.js
@@ -1,18 +1,29 @@
-const router = require("express").Router()
-const controllers = require("../middlewares/controllers")
-const TransactionService = require("../services/transaction")
-const fileUploder = require("../lib/uploader")
-const { authorizedTokenAdmin } = require("../middlewares/authMiddlewareAdmin")
-const { authorizedToken } = require("../middlewares/authMiddleware")
+const router = require("express").Router();
+const controllers = require("../middlewares/controllers");
+const TransactionService = require("../services/transaction");
+const fileUploder = require("../lib/uploader");
+const { authorizedTokenAdmin } = require("../middlewares/authMiddlewareAdmin");
+const { authorizedToken } = require("../middlewares/authMiddleware");
 
-router.post("/prescription",
-    authorizedToken,
-    fileUploder({
-        destinationFolder: "prescriptions",
-        fileType: "image",
-        prefix: "POST"
-    }).array("prescriptions"), controllers(TransactionService.newTransactionByPrescription))
-router.get("/", authorizedTokenAdmin, controllers(TransactionService.getAllTransaction))
-router.post("/prescription/new-items", authorizedTokenAdmin, controllers(TransactionService.addTransactionItemsByAdmin))
+router.post(
+  "/prescription",
+  authorizedToken,
+  fileUploder({
+    destinationFolder: "prescriptions",
+    fileType: "image",
+    prefix: "POST",
+  }).array("prescriptions"),
+  controllers(TransactionService.newTransactionByPrescription)
+);
+router.get(
+  "/",
+  authorizedTokenAdmin,
+  controllers(TransactionService.getAllTransaction)
+);
+router.post(
+  "/prescription/new-items",
+  authorizedTokenAdmin,
+  controllers(TransactionService.addTransactionItemsByAdmin)
+);
 
-module.exports = router
+module.exports = router;
