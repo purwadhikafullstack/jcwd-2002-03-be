c4bf051d4bdd177015126d09dc6ca1ca6e687a0f
akimajid <akimajid07@gmail.com>
2022-07-15 13:25:12 +0700

    Feat/[P3-45] (#31)

--- src/routes/inventory.js
+++ src/routes/inventory.js
@@ -4,5 +4,6 @@ const controllers = require("../middlewares/controllers")
 const inventoryService = require("../services/inventory")
 
 router.post("/add-stock", authorizedToken, controllers(inventoryService.addStockProduct))
+router.get("/sales-report", controllers(inventoryService.getSalesReport))
 
 module.exports = router
--- src/services/inventory/index.js
+++ src/services/inventory/index.js
@@ -1,11 +1,18 @@
 const Service = require("../service");
-const { Product, Inventory, Stock_opname } = require("../../lib/sequelize");
+const {
+  Product,
+  Inventory,
+  Stock_opname,
+  Transaction,
+  Transaction_items,
+} = require("../../lib/sequelize");
 const { Op } = require("sequelize");
 
 class inventoryService extends Service {
   static addStockProduct = async (req) => {
     try {
-      const { quantity, expired_date, ProductId, buying_price } = req.body;
+      const { quantity, expired_date, ProductId, buying_price, type } =
+        req.body;
       console.log(req.body);
 
       const addStock = await Inventory.create({
@@ -13,6 +20,7 @@ class inventoryService extends Service {
         expired_date,
         ProductId,
         buying_price,
+        type,
         AdminId: req.token.id,
       });
 
@@ -33,6 +41,62 @@ class inventoryService extends Service {
       });
     } catch (err) {
       console.log;
+      this.handleError({
+        message: "Server error",
+        statusCode: 500,
+      });
+    }
+  };
+  static getSalesReport = async (req) => {
+    try {
+      const { _limit = 30, _page = 1, _sortBy = "", _sortDir = "", searchProduct, } = req.query;
+
+      delete req.query._limit;
+      delete req.query._page;
+      delete req.query._sortBy;
+      delete req.query._sortDir;
+      delete req.query.searchProduct;
+
+      let searchByNameClause = {};
+
+      if (searchProduct) {
+        searchByNameClause = {
+          med_name: { [Op.like]: `%${searchProduct}%` },
+        };
+      }
+
+      const findSalesReport = await Inventory.findAndCountAll({
+        where: {
+          ...req.query,
+        //   ...searchByNameClause,
+        },
+        limit: _limit ? parseInt(_limit) : undefined,
+        offset: (_page - 1) * _limit,
+        // order: _sortBy ? [[_sortBy, _sortDir]] : undefined,
+        order: [["createdAt", "DESC"]],
+        distinct: true,
+        include: [
+          {
+            model: Transaction,
+            include: Transaction_items,
+          },
+          {
+            model: Product,
+            where: {
+                ...req.query,
+                ...searchByNameClause,
+              },
+          }
+        ],
+      });
+
+      return this.handleSuccess({
+        message: "Sales report found",
+        statusCode: 200,
+        data: findSalesReport,
+      });
+    } catch (err) {
+      console.log(err);
       this.handleError({
         message: "Server error",
         statusCode: 500,
